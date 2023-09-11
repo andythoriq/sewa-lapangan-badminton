4bb51e98c9574f5f9a483a4e42255daf66c45b59
andythoriq <andyythoriqq@gmail.com>
2023-09-11 16:27:06 +0700

    p

--- api/app/Traits/CollideCheck.php
+++ api/app/Traits/CollideCheck.php
@@ -23,7 +23,7 @@ trait CollideCheck
                 ($existingStart->between($newStart, $newFinish) || $existingFinish->between($newStart, $newFinish))
             ) {
                 throw ValidationException::withMessages([
-                    'start' => ['Start and Finish collide with existing ones. Add one second to avoid collision.'],
+                    'start' => ['Start and Finish collide with existing ones.'],
                 ]);
             }
         }
