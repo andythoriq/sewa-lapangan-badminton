71400601b75735e75da7b8fca39adffc83c0a2ca
Tanti Dwi Juliyanti <tdwijuliyanti@gmail.com>
2023-09-14 10:54:13 +0700

    Memperbaiki path rush hour

--- ui/src/Pages/Users/DataMaster/Rush Hour/rush.js
+++ ui/src/Pages/Users/DataMaster/Rush Hour/rush.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Row, Col, Container, Card } from "react-bootstrap";
+import { Row, Col, Card, Form } from "react-bootstrap";
 import FormSelect from "../../../../Components/Form/select";
 import FormSelectTime from "../../../../Components/Form/selectTime";
 
@@ -16,28 +16,30 @@ const Rush = () => {
       <h4 className="mb-4">
         <b>Rush Hour</b>
       </h4>
-      <Container>
-        <Form>
-          <Row>
-            <Card>
-              <Col className="col-12 col-md-6">
-                <FormSelect name="court" label="Court" className="form-select form-select-sm" options={dataCourt} />
-              </Col>
-              <Col className="col-12 col-md-6">
-                <FormSelectTime label="Start" nameHour="start_hour" nameMinute="start_minute" nameTime="start_time" />
-              </Col>
-              <Col className="col-12 col-md-6">
-                <FormSelectTime label="End" nameHour="end_hour" nameMinute="end_minute" nameTime="end_time" />
-              </Col>
-              <Col className="col-12 text-right mt-4">
-                <button type="button" className="btn btn-danger btn-sm me-md-6">
-                  Save
-                </button>
-              </Col>
-            </Card>
-          </Row>
-        </Form>
-      </Container>
+      <Row>
+        <Col className="col-sm-6 m-auto">
+          <Card className="p-4 mt-5">
+            <Form>
+              <Row>
+                <Col className="col-6 col-md-6">
+                  <FormSelect name="court" label="Court" className="form-select form-select-sm" options={dataCourt} />
+                </Col>
+                <Col className="col-12 col-md-8">
+                  <FormSelectTime label="Start" nameHour="start_hour" nameMinute="start_minute" nameTime="start_time" />
+                </Col>
+                <Col className="col-12 col-md-8">
+                  <FormSelectTime label="End" nameHour="end_hour" nameMinute="end_minute" nameTime="end_time" />
+                </Col>
+                <Col className="mt-5 mb-4">
+                  <button type="button" className="btn btn-danger btn-sm">
+                    Save
+                  </button>
+                </Col>
+              </Row>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
 };
